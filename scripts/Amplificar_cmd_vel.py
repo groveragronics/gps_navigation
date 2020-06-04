f2b8f7a95b42bcab03da0f0fa3195cff490e0337
Atocha-9 <jesusdat9@outlook.es>
2020-06-04 23:15:40 +0000

    Actualicé gps_navigation a una mejor version que en conjunto con el Amplificador permiten al UMA moverse mejor dentro del invernadero

--- scripts/Amplificar_cmd_vel.py
+++ scripts/Amplificar_cmd_vel.py
@@ -1,42 +1,34 @@
 #!/usr/bin/env python  
 import rospy  
 import math
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist #Importar tipo de mensaje que usa cmd_vel 
 
 class GPSPubClass():  
     def __init__(self):  
         rospy.on_shutdown(self.cleanup)   
         ############################### SUBSCRIBERS #####################################  
-        rospy.Subscriber('nav2d_vel', Twist, self.get_cmd)
+        #Subscriber que toma los valores de velocidad de operator para posteriormente hacerlos mas grandes
+        rospy.Subscriber('nav2d_vel', Twist, self.get_cmd) 
         ####  PUBLISHERS ###
         self.cmd_vel_pub=rospy.Publisher("cmd_vel", Twist, queue_size = 1)
-        
         ### CONSTANTS ###
         self.robot_vel=Twist()
         self.robot_vel.linear.x=0
         self.robot_vel.angular.z=0
-
         r = rospy.Rate(10) #10 Hz 
         #********** INIT NODE **********###  
         while not rospy.is_shutdown():
-            self.cmd_vel_pub.publish(self.robot_vel)
+            self.cmd_vel_pub.publish(self.robot_vel) #Publicar valores de velocidad ya incrementados 
             r.sleep()
       
     def get_cmd (self, msg):
-
-        self.robot_vel.linear.x = 1.1 * msg.linear.x
-        self.robot_vel.angular.z = 8 * msg.angular.z
-        if (abs(8 * msg.angular.z) < 0.1 and abs(8 * msg.angular.z) > 0):
-            self.robot_vel.linear.x = 3.2 * msg.linear.x
-        print self.robot_vel.linear.x 
-        print self.robot_vel.angular.z
+        #Se leen valores de velocidad de nav2d_vel para multiplicarse y hacer que UMA se mueva mejor
+        self.robot_vel.linear.x = 1.2 * msg.linear.x 
+        self.robot_vel.angular.z = 4 * msg.angular.z
      
     def cleanup(self):  
 
-        #This function is called just before finishing the node  
-        # You can use it to clean things up before leaving  
-        # Example: stop the robot before finishing a node.   
-        print("entro")
+        #Esta funcion es ejecutada antes de que el nodo se cierre, se usa para detener al UMA  
         self.robot_vel.angular.z = 0
         self.robot_vel.linear.x = 0 
         self.cmd_vel_pub.publish(self.robot_vel)
--- scripts/gps_navigation.py
+++ scripts/gps_navigation.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python  
 import rospy  
-import math     #to calculate the tan inverse 
-from sensor_msgs.msg import NavSatFix 
-from sensor_msgs.msg import Imu #Odometry   #odometry like imu
-from tf.transformations import euler_from_quaternion #library to transform the quaternion data of imu to euler angles
-from nav2d_operator.msg import cmd
+import math     #Para poder usar la tangente inversa, abs()... 
+from sensor_msgs.msg import NavSatFix #Importar tipo de mensaje que usa el GPS
+from sensor_msgs.msg import Imu #Importar tipo de mensaje IMU
+from tf.transformations import euler_from_quaternion #Libreria para tranformar los datos del IMU de cuaterniones a angulos de Euler
+from nav2d_operator.msg import cmd #Importar tipo de mensaje a publicar a operator
 
 class GPSPubClass():  
     def __init__(self):  
         rospy.on_shutdown(self.cleanup)   
         ############################### SUBSCRIBERS #####################################  
-        rospy.Subscriber("fix", NavSatFix, self.gps_cb)   
+        # Subscriber para leer el valor del gps del UMA, gps del goal, valor del IMU  
+        rospy.Subscriber("fix", NavSatFix, self.gps_cb)
         rospy.Subscriber("fixgoal", NavSatFix, self.gps_goal_cb)
         rospy.Subscriber('imu', Imu, self.get_rotation)
         ####  PUBLISHERS ###
+        #Publica velocidad a cmd el cual recibe operator para la evasion de obstaculos
         self.cmd_pub=rospy.Publisher("cmd", cmd, queue_size = 1)
-        
         ### CONSTANTS ###
         self.robot_vel=cmd()
-        self.robot_vel.Velocity = 0
-        self.robot_vel.Turn = 0
+        self.robot_vel.Velocity=0
+        self.robot_vel.Turn=0
         r = rospy.Rate(10) #10 Hz 
         self.dif_latitude=0.0
         self.dif_longitude=0.0
@@ -29,100 +30,88 @@ class GPSPubClass():
         self.longitude_goal=0.0
         #********** INIT NODE **********###  
         while not rospy.is_shutdown():
-            self.cmd_pub.publish(self.robot_vel)
+            self.cmd_pub.publish(self.robot_vel) #Publicando datos a cmd_pub a una frecuencia de 10Hz
             r.sleep()
-       #cuando no hay while
-        #rospy.spin() 
-
 
     def gps_cb(self, msg):  
-        
+        #Se obtienen valores de latitud y longitud del GPS del robot (topico fix)   
         self.latitude_robot= msg.latitude
-        self.longitude_robot= msg.longitude 
+        self.longitude_robot= msg.longitude + (-0.000009594 * 0.165) #Correccion de longitud por posicion de GPS en el UMA
 
     def gps_goal_cb(self, msg):  
-        ## This function receives a number
+        #Se obtienen valores de latitud y longitud del GPS goal (topico fixgoal) y se calcula la diferencia que hay respecto al goal
         self.latitude_goal= msg.latitude
         self.longitude_goal= msg.longitude
         self.dif_latitude= self.latitude_goal - self.latitude_robot
         self.dif_longitude= self.longitude_goal - self.longitude_robot
-        #print("latitud goal: ", msg.latitude)
     
     def get_rotation (self, msg):
+        #Transformacion de valores de IMU de cuaterniones a angulos de Euler
         orientation_q = msg.orientation
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
         (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-        #print("dif latitude: ", self.dif_latitude)
-        #print("dif longitude: ", self.dif_longitude)
-        #print yaw
         
-        #Computing the angle to de goal
-        ##Angulos de cuadrantes 1 y 2 respecto a x. En radianes de 0 a Pi (0 -> 180)##
-        ##Angulos de cuadrantes 2 y 4 respecto a x. En radianes de 0 a -Pi (0 -> -180)##
-        #print("yaw: ", yaw)
-
-        angle_1 = -1* math.atan2(self.dif_longitude, (self.dif_latitude + 0.0000000000000000001) )
-        print("angle_1: ", angle_1) 
-        
+        #Calculo del angulo del goal respecto al UMA
+        #Angulos de cuadrantes 1 y 2 respecto a x. En radianes de 0 a Pi (0 -> 180)
+        #Angulos de cuadrantes 2 y 4 respecto a x. En radianes de 0 a -Pi (0 -> -180)
+        angle_goal = -1* math.atan2(self.dif_longitude, (self.dif_latitude + 0.0000000000000000001) )
+      
         ## Movimiento de motores para llegar al objetivo
-        kdist = 25000        
-        kang = -0.19 #Para mantener los valores de angulo en el rango de -1 y 1 que son los valores que ocupa el cmd 
-        vmax = 1.0
-        latlong = 0.00001317690754  ## 1m se representa como 0.00001317690754 en terminos de  latitude y longitude 
-        dif_angle = angle_1 - yaw
-        #print dif_angle
-        print("angulo robot: ", yaw)
-        print ("dif_angle:", dif_angle)
-        ## Computing the distance to the goal
+        kdist = 25000 #La constante por la que se multiplica la diferencia de distancia (en terminos de lat y long) entre UMA y goal        
+        kang = -0.20  #Para dejar los valores de velocidad angular en rango de -1 a 1 que son los que lee Turn en cmd
+        vmax = 1.0    #Para dejar los valores de velocidad lineal en terminos de -1 a 1 que son los que lee Velocity en cmd
+        latlong = 0.00001317690754 #1m se representa como 0.00001317690754 en terminos de  latitude y longitude aproximadamente
+        #Calculo de la diferencia de angulo del goal respecto al frente del UMA usando yaw valor generado por el IMU del UMA.  
+        dif_angle = angle_goal - yaw
+        ## Calculo de distancia al goal
         dist_to_goal = ((self.dif_latitude ** 2) + (self.dif_longitude ** 2)) ** 0.5  
-  
-
-        self.robot_vel.Turn =  kang * dif_angle
-         
-        if (abs(dif_angle) > 0.2 and abs(dif_angle) < 0.25 ):
-            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.000001
-            self.robot_vel.Turn=  kang * dif_angle
-        elif (abs(dif_angle) < 6 and abs(dif_angle) > 5 ): 
-            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.000001
-            self.robot_vel.Turn =  kang * -0.5 * dif_angle
-        elif (abs(dif_angle) < 5 and abs(dif_angle) > 0.25 ): 
-                self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.000001
-                self.robot_vel.Turn =  kang* dif_angle
-                if(abs(angle_1) > 3 and abs(angle_1) < 3.16): 
-                    self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) +0.000001
-                    self.robot_vel.Turn =  kang* abs(dif_angle)  
-                  
+       
+        #Evaluacion de casos especiales en donde la diferencia de angulo no corresponde al valor necesario para girar hacia el goal. 
+        if (angle_goal < -0.07 and angle_goal > -1.50 and yaw > 2.355): 
+            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.1
+            self.robot_vel.Turn =  kang * (dif_angle + 6.28)
+        elif (angle_goal < -1.64 and yaw > 0.785): 
+            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.1
+            self.robot_vel.Turn =  kang * (dif_angle + 6.28)
+        elif (angle_goal > 1.64 and yaw < -0.785): 
+            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.1
+            self.robot_vel.Turn =  kang * (dif_angle - 6.28)
+        elif (angle_goal > 0.07 and angle_goal < 1.50 and yaw < -2.355): 
+            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.1
+            self.robot_vel.Turn =  kang * (dif_angle - 6.28)
         else:
-            self.robot_vel.Turn = 0.0
-        
-            ## lo dividi entre 2 para quedarme a 50cm de distancia de separacion del goal  y con un rango de 500m    
-        if(dist_to_goal  > (latlong/2.0) and dist_to_goal  < (latlong*500)):
-            self.robot_vel.Velocity = (self.robot_vel.Velocity + 0.3)
-            if (self.robot_vel.Velocity >= (kdist*dist_to_goal) ): 
-                self.robot_vel.Velocity = (kdist*dist_to_goal)
-            if (self.robot_vel.Velocity >= vmax):
-                self.robot_vel.Velocity = vmax
-        else: 
-            if (self.robot_vel.Turn != 0): 
-                 self.robot_vel.Velocity = 0.00000001
+            self.robot_vel.Velocity = (self.robot_vel.Velocity * 0.6) + 0.1
+            self.robot_vel.Turn =  self.robot_vel.Turn =  kang * dif_angle
+        #Si la velocidad angular de UMA es pequeña puede incrementar su velocidad lineal     
+        if( (abs(self.robot_vel.Turn)) < 0.1 ): 
+            #El UMA se queda a 50cm de distancia de separacion del goal y no se mueva si el objetivo esta a mas de 500m de distancia
+            if(dist_to_goal  > (latlong/2.0) and dist_to_goal  < (latlong*500)):
+                self.robot_vel.Velocity = self.robot_vel.Velocity + 0.3 
+                if (self.robot_vel.Velocity >= (kdist*dist_to_goal) ): 
+                    self.robot_vel.Velocity = kdist*dist_to_goal
+                if (self.robot_vel.Velocity >= vmax):
+                    self.robot_vel.Velocity = vmax
             else:
-                self.robot_vel.Velocity = 0.0  
+                if ( (abs(self.robot_vel.Turn)) != 0.0 ): 
+                    self.robot_vel.Velocity = 0.001 #Al publicar a cmd ocupa que velocity sea mayor a 0 para poder seguir girando
+                else:
+                    self.robot_vel.Velocity = 0.0    
 
+  
     def cleanup(self):  
 
-        #This function is called just before inishing the node  
-        # You can use it to clean things up before leaving  
-        # Example: stop the robot before finishing a node.   
-        print("entro")
-        self.robot_vel.Velocity = 0.0
-        self.robot_vel.Turn = 0.0 
+        #Esta funcion es ejecutada antes de que el nodo se cierre, se usa para detener al UMA   
+        self.robot_vel.Turn = 0
+        self.robot_vel.Velocity = 0 
         self.cmd_pub.publish(self.robot_vel)
+
+      
 
 ############################### MAIN PROGRAM ####################################  
 
 if __name__ == "__main__":  
 
-    rospy.init_node("Navegacion_gps_V3", anonymous=True)  
+    rospy.init_node("gps_navigation_V5", anonymous=True)  
 
     try:  
 
@@ -130,4 +119,4 @@ if __name__ == "__main__":
 
     except:  
 
-        rospy.logfatal("Navegacion_gps_V3 died")  
+        rospy.logfatal("gps_navigation_V5 died")  
